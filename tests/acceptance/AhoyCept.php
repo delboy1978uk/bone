f3fe39670f6d546e5fc182c0e423c65b1d71f0b2
Derek McLean <delboy1978uk@gmail.com>
2014-11-21 14:29:18 +0000

    Moved acceptance tests into Bone MVC Skeleton

--- tests/acceptance/AhoyCept.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$I = new AcceptanceTester($scenario);
-$I->wantTo("ensure th' ship is docked in the Bay of Hoempaige");
-$I->amOnPage('/');
-$I->see('Bone MVC Framework');
--- tests/acceptance/LostCept.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$I = new AcceptanceTester($scenario);
-$I->wantTo("ensure we get 404 messages!");
-$I->amOnPage('/shiver/me/timbers');
-$I->see('LOST AT SEA');
-$I->amOnPage('/kraken');
-$I->see('LOST AT SEA');
-$I->amOnPage('/davie/jones');
-$I->see('LOST AT SEA');
