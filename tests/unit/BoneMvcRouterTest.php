70723bb5de016ce0028c85a125563c43247cd812
delboy1978uk <delboy1978uk@gmail.com>
2014-12-06 21:35:07 +0000

    code completion fer router cap'n!

--- tests/unit/BoneMvcRouterTest.php
+++ tests/unit/BoneMvcRouterTest.php
@@ -11,21 +11,24 @@ class BoneMvcRouterTest extends \Codeception\TestCase\Test
     */
     protected $tester;
 
+    protected $get;
+    protected $post;
+    protected $cookie;
+    protected $server;
+    protected $routes;
+
     protected $request;
     protected $router;
     protected $registry;
 
     protected function _before()
     {
-        $get = array('getParam1' => 'getParam1Value');
-        $post = array('postParam1' => 'postParam1Value');
-        $cookie = array('cookieParam1' => 'cookieParam1Value');
-        $server = array('REQUEST_URI' => '/test',
-                        'REQUEST_METHOD' => 'POST');
-
-        $this->request = new Request( $get, $post, $cookie, $server);
-        $this->registry = Registry::ahoy();
-        $this->registry->set('routes', array(
+        $this->get = array('getParam1' => 'getParam1Value');
+        $this->post = array('postParam1' => 'postParam1Value');
+        $this->cookie = array('cookieParam1' => 'cookieParam1Value');
+        $this->server = array('REQUEST_URI' => '/test',
+                              'REQUEST_METHOD' => 'POST');
+        $this->routes = array(
             '/' => array(
                 'controller' => 'index',
                 'action' => 'index',
@@ -39,8 +42,12 @@ class BoneMvcRouterTest extends \Codeception\TestCase\Test
                     'speak' => 'pirate',
                 ),
             ),
-        ));
-        $this->router = new Router($this->request);
+        );
+
+        $this->registry = Registry::ahoy();
+        $this->registry->set('routes', $this->routes);
+
+        $this->request = new Request( $this->get, $this->post, $this->cookie, $this->server);
 
     }
 
@@ -51,12 +58,44 @@ class BoneMvcRouterTest extends \Codeception\TestCase\Test
     // tests
     public function testParseReturnsResponse()
     {
+        $this->router = new Router($this->request);
         $this->assertInstanceOf('Bone\Mvc\Request',$this->router->dispatch());
     }
 
-    public function test()
+    public function testControllerMatch()
     {
+        $this->server['REQUEST_URI'] = '/the-lone-pirate';
+        $this->request = new Request( $this->get, $this->post, $this->cookie, $this->server);
+        $this->router = new Router($this->request);
+        $request = $this->router->dispatch();
+        $this->assertInstanceOf('Bone\Mvc\Request', $request);
+    }
 
+    public function testControllerActionMatch()
+    {
+        $this->server['REQUEST_URI'] = '/treasure/chest';
+        $this->request = new Request( $this->get, $this->post, $this->cookie, $this->server);
+        $this->router = new Router($this->request);
+        $request = $this->router->dispatch();
+        $this->assertInstanceOf('Bone\Mvc\Request', $request);
+    }
+
+    public function testControllerActionParamsMatch()
+    {
+        $this->server['REQUEST_URI'] = '/treasure/chest/value/100/contents/gold';
+        $this->request = new Request( $this->get, $this->post, $this->cookie, $this->server);
+        $this->router = new Router($this->request);
+        $request = $this->router->dispatch();
+        $this->assertInstanceOf('Bone\Mvc\Request', $request);
+    }
+
+    public function testHomePageMatch()
+    {
+        $this->server['REQUEST_URI'] = '/';
+        $this->request = new Request( $this->get, $this->post, $this->cookie, $this->server);
+        $this->router = new Router($this->request);
+        $request = $this->router->dispatch();
+        $this->assertInstanceOf('Bone\Mvc\Request', $request);
     }
 
 }
