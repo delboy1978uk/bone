12c5f996bb5b084e328acca8abc97a33b0c7c8a9
delboy1978uk <delboy1978uk@gmail.com>
2018-10-25 03:32:45 +0200

    more tests

---
+++ tests/unit/Traits/HasAttributesTraitTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Bone\Server\Environment;
+use Codeception\TestCase\Test;
+
+class HasAttributesTraitTest extends Test
+{
+    const CONFIG_FOLDER = 'tests/_data/config';
+
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    public function testGetSetAttributes()
+    {
+        $server = new Environment([]);
+        $server->setAttributes([
+            'test' => 'test1',
+            'hello' => 'world',
+        ]);
+        $this->assertCount(2, $server->getAttributes());
+        $this->assertEquals('test1', $server->getAttribute('test'));
+        $this->assertEquals('world', $server->getAttribute('hello'));
+        $server->setAttribute('another', 'value');
+        $this->assertCount(3, $server->getAttributes());
+        $this->assertEquals('value', $server->getAttribute('another'));
+    }
+}
