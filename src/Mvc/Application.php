2af42bfac472208c788ed002274f28259c7ec7da
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-06-01 03:05:37 +0200

    adding modules

--- src/Mvc/Application.php
+++ src/Mvc/Application.php
@@ -2,9 +2,13 @@
 
 namespace Bone\Mvc;
 
+use Barnacle\Container;
+use Barnacle\RegistrationInterface;
 use Bone\Server\Environment;
+use BoneMvc\Module\Dragon\Controller\DragonController;
 use League\Route\Router;
-use Pimple\Container;
+use League\Route\Strategy\ApplicationStrategy;
+use PDO;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\ServerRequestFactory;
@@ -13,9 +17,6 @@ use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
 class Application
 {
-    /** @var Registry $registry */
-    private $registry;
-
     /** @var Container $registry */
     private $treasureChest;
 
@@ -40,15 +41,13 @@ class Application
      * @param array $config
      * @return Application
      */
-    public static function ahoy(array $config = [])
+    public static function ahoy()
     {
         static $inst = null;
         if ($inst === null)
         {
             $inst = new Application();
-            $inst->registry = Registry::ahoy();
             $inst->treasureChest = new Container();
-            $inst->setConfig($config);
             $env = getenv('APPLICATION_ENV');
             if ($env) {
                 $inst->setEnvironment($env);
@@ -60,13 +59,59 @@ class Application
     /**
      * @param array $config
      */
-    private function setConfig(array $config)
+    private function configureContainer(array $config)
     {
+        $c = $this->treasureChest;
+
+        // add the config array
         foreach($config as $key => $value)
         {
-            $this->registry->set($key,$value);
-            $this->treasureChest[$key] = $value;
+            $c[$key] = $value;
         }
+
+        // set up a db connection
+        $c[PDO::class] = $c->factory(function(Container $c): PDO {
+            $credentials = $c->get('db');
+            $host =$credentials['host'];
+            $db = $credentials['database'];
+            $user = $credentials['user'];
+            $pass = $credentials['pass'];
+
+            $dbConnection = new PDO('mysql:host='.$host.';dbname='.$db, $user, $pass, [
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            ]);
+
+            return $dbConnection;
+        });
+
+        $modules = $c->get('modules');
+        $packages = $c->get('packages');
+
+        foreach ($modules as $module) {
+            $packageName  = '\BoneMvc\Module\\'.$module.'\\'.$module . 'Package';
+            if (class_exists($packageName)) {
+                /** @var RegistrationInterface $package */
+                $package = new $packageName();
+                if ($package->hasEntityPath()) {
+                    $c['entity_paths'][] = $package->getEntityPath();
+                }
+                $package->addToContainer($c);
+            }
+        }
+
+        foreach ($packages as $module) {
+            $packageName  = '\BoneMvc\Module\\'.$module.'\\'.$module . 'Package';
+            if (class_exists($packageName)) {
+                /** @var RegistrationInterface $package */
+                $package = new PackageName();
+                if ($package->hasEntityPath()) {
+                    $c['entity_paths'][] = $package->getEntityPath();
+                }
+                $package->addToContainer($c);
+            }
+        }
+
     }
 
     /**
@@ -78,31 +123,45 @@ class Application
      */
     public function setSail()
     {
+        // load in the config and set up the dependency injection container
         $env = new Environment($_SERVER);
-        if (!count($this->registry->getAll())) {
-            $config = $env->fetchConfig($this->configFolder, $this->environment);
-            $this->setConfig($config);
-        }
-        $request = ServerRequestFactory::fromGlobals($_SERVER, $_GET, $_POST, $_COOKIE, $_FILES);
-        $response = new Response();
+        $config = $env->fetchConfig($this->configFolder, $this->environment);
+        $this->configureContainer($config);
 
         $router = new Router();
+        $strategy = (new ApplicationStrategy())->setContainer($this->treasureChest);
+        $router->setStrategy($strategy);
+        $this->setRoutes($router, $config['routes']);
 
-        $router->map('GET', '/', function (ServerRequestInterface $request) : ResponseInterface {
-            $response = new \Zend\Diactoros\Response;
-            $response->getBody()->write('<h1>Hello, World!</h1>');
-            return $response;
-        });
-
+        $request = ServerRequestFactory::fromGlobals($_SERVER, $_GET, $_POST, $_COOKIE, $_FILES);
         $response = $router->dispatch($request);
 
-        // send the response to the browser
+        // send the response
         (new SapiEmitter)->emit($response);
 
 //        $dispatcher = new Dispatcher($request, $response, $env);
 //        $dispatcher->fireCannons();
 
         return true;
+    }
+
+    /**
+     * @param Router $router
+     * @param array $config
+     * @return Router
+     */
+    private function setRoutes(Router $router, array $config): Router
+    {
+        /** @todo add the real routs */
+        $router->map('GET', '/', function (ServerRequestInterface $request) : ResponseInterface {
+            $response = new Response;
+            $response->getBody()->write('<h1>Hello, World!</h1>');
+            return $response;
+        });
+
+        $router->map('GET', '/dragon', [DragonController::class, 'indexAction']);
+
+        return $router;
     }
 
     /**
