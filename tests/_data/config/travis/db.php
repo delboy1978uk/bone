e95ad3f5d97b40144412578fc093683fc7a95d5d
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-09-20 17:28:21 +0200

    travis db credentials

--- tests/_data/config/travis/db.php
+++ tests/_data/config/travis/db.php
@@ -3,7 +3,7 @@
 return [
     'db' => [
         'driver' => 'pdo_mysql',
-        'host' => 'mariadb',
+        'host' => 'locaslhost',
         'database' => 'bone_db',
         'dbname' => 'bone_db',
         'user' => 'travis',
