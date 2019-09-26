df131a43a7b6557e940249a86c4544559f3678ce
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-09-26 20:25:47 +0200

    pass array to refactored helper in test

--- tests/unit/View/Helper/AlertBoxTest.php
+++ tests/unit/View/Helper/AlertBoxTest.php
@@ -12,7 +12,7 @@ class AlertBoxTest extends Test
     {
         $this->assertEquals('<div class="alert alert-info"><button type="button" class="close" data-dismiss="alert" aria-label="Close">
   <span aria-hidden="true">&times;</span>
-</button>crack open the rum</div>', AlertBox::alertBox('crack open the rum'));
+</button>crack open the rum</div>', AlertBox::alertBox(['crack open the rum']));
         $this->assertEquals('<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-label="Close">
   <span aria-hidden="true">&times;</span>
 </button>not one message<br />but two!</div>', AlertBox::alertBox([
