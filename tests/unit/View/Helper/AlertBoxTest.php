5a3865f759800c487b4fb644cb6d97e6fd3811f4
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-09-26 21:44:13 +0200

    more whitespace tweaking

--- tests/unit/View/Helper/AlertBoxTest.php
+++ tests/unit/View/Helper/AlertBoxTest.php
@@ -12,11 +12,11 @@ class AlertBoxTest extends Test
     {
         $helper = new AlertBox();
         $this->assertEquals('<div class="alert alert-info"><button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>crack open the rum</div>', $helper->alertBox(['crack open the rum']));
+                      <span aria-hidden="true">&times;</span>
+                    </button>crack open the rum</div>', $helper->alertBox(['crack open the rum']));
         $this->assertEquals('<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>not one message<br />but two!</div>', $helper->alertBox([
+                      <span aria-hidden="true">&times;</span>
+                    </button>not one message<br />but two!</div>', $helper->alertBox([
             'not one message',
             'but two!',
             'danger',
