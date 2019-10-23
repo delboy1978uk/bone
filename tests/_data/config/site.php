2d2f20bbc929f39ee5713289a65a79de4494fea3
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-10-23 20:07:56 +0200

    fix typo

--- tests/_data/config/site.php
+++ tests/_data/config/site.php
@@ -9,7 +9,7 @@ return [
         'serverEmail' => 'noreply@awesome.scot',
         'company' => 'Pirates Inc.',
         'address' => '1 Big Tree, Booty Island',
-        'lgo' => '/img/skull_and_crossbones.png',
+        'logo' => '/img/skull_and_crossbones.png',
         'emailLogo' => '/img/emails/logo.png',
     ],
 ];
