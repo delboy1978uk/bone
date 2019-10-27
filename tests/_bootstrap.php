93c9c79b2a33d44b16ecfc693f19d0b6f78efa44
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-10-27 02:04:38 +0100

    move bootstrap

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -3,9 +3,3 @@
 
 require_once __DIR__.'/../vendor/autoload.php'; // composer autoload
 
-$_SERVER = [
-    'SERVER_NAME' => 'sinking.ship',
-    'REMOTE_ADDR' => '192.168.6.6',
-    'HTTP_USER_AGENT' => 'Some brand new smart phone',
-];
-
--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,3 +1,9 @@
 <?php
 
 define('APPLICATION_PATH', '.');
+
+$_SERVER = [
+    'SERVER_NAME' => 'sinking.ship',
+    'REMOTE_ADDR' => '192.168.6.6',
+    'HTTP_USER_AGENT' => 'Some brand new smart phone',
+];
