7772bcfa9e7f935d00baf6024d597c22b629b4d6
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-10-27 02:36:06 +0100

    fix time issue

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -6,5 +6,5 @@ $_SERVER = [
     'SERVER_NAME' => 'sinking.ship',
     'REMOTE_ADDR' => '192.168.6.6',
     'HTTP_USER_AGENT' => 'Some brand new smart phone',
-    'REQUEST_TIME_FLOAT' => microtime(),
+    'REQUEST_TIME_FLOAT' => microtime(true),
 ];
